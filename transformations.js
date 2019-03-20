931556172cf0e672e0065ae20b97b68bf705e7ce
Eric Huang <ericshuang23@gmail.com>
2019-03-19 19:31:14 -0700

    Fixed Rotations and Translations
    - Translations now work on global XYZ axes
    (previously translations worked on the bunny's axes)
    - Rotations now have the bunny as the center

--- transformations.js
+++ transformations.js
@@ -15,3 +15,58 @@ function identity4(){
     identityMatrix[3][3] = 1;
     return identityMatrix;
 }
+
+//used in the point camera
+function wireFrameCubeLines(){
+    var vertices = [
+        vec4(-0.5, -0.5,  1.5, 1.0),    //front bottom left
+        vec4(-0.5,  0.5,  1.5, 1.0),    //front top left
+        vec4(0.5,  0.5,  1.5, 1.0),     //front top right
+        vec4(0.5, -0.5,  1.5, 1.0),     //front bottom right
+        vec4(-0.5, -0.5, 0.5, 1.0),     //back bottom left
+        vec4(-0.5,  0.5, 0.5, 1.0),     //back top left
+        vec4(0.5,  0.5, 0.5, 1.0),      //back top right
+        vec4( 0.5, -0.5, 0.5, 1.0)      //back bottom right
+    ];
+
+    var points = [];
+    //front bottom-left to front top-left
+    points.push(vertices[0]);
+    points.push(vertices[1]);
+    //front bottom-left to front bottom right
+    points.push(vertices[0]);
+    points.push(vertices[3]);
+    //front bottom-left to back bottom-left
+    points.push(vertices[0]);
+    points.push(vertices[4]);
+    //front top-left to front top-right
+    points.push(vertices[1]);
+    points.push(vertices[2]);
+    //front top-left to back top-left
+    points.push(vertices[1]);
+    points.push(vertices[5]);
+    //front top-right to front bottom-right
+    points.push(vertices[2]);
+    points.push(vertices[3]);
+    //front top-right to back top-right
+    points.push(vertices[2]);
+    points.push(vertices[6]);
+    //front bottom-right to back bottom-right
+    points.push(vertices[3]);
+    points.push(vertices[7]);
+    //back top-left to back-top right
+    points.push(vertices[5]);
+    points.push(vertices[6]);
+    //back top-left to back bottom-left
+    points.push(vertices[5]);
+    points.push(vertices[4]);
+    //back top-right to back bottom-right
+    points.push(vertices[6]);
+    points.push(vertices[7]);
+    //back bottom-left to back bottom-right
+    points.push(vertices[4]);
+    points.push(vertices[7]);
+
+    console.log(points.length);
+    return points;
+}
